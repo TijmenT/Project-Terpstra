87d3753a4727576890502ce1d2ccc4efc9f7a9a6
TijmenT <30957@ma-web.nl>
2022-07-04 15:25:06 +0200

    final commit

--- _private/controllers/LoginController.php
+++ _private/controllers/LoginController.php
@@ -5,7 +5,7 @@ namespace Website\Controllers;
 /**
  * Class WebsiteController
  *
- * Deze handelt de logica van de homepage af
+ * Deze handelt de logica van de homepage af.
  * Haalt gegevens uit de "model" laag van de website (de gegevens)
  * Geeft de gegevens aan de "view" laag (HTML template) om weer te geven
  *
